2104790c71d2c2c78ecc46e70b06fde3c50eff58
Fellipe C.T De Camargo <fellipectcamargo@gmail.com>
2024-11-25 10:13:21 -0400

    test

---
+++ hrm_project/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module hrm.hrm_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens hrm.hrm_project to javafx.fxml;
+    exports hrm.hrm_project;
+}
